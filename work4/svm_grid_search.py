2bdd8c8372065174e04d17860253e378acbdbc77
masa3141 <masahiro@kazama.tv>
2014-07-14 14:00:45 +0900

    add work4/svm_grid_search.py

--- work4/svm_grid_search.py
+++ work4/svm_grid_search.py
@@ -2,11 +2,12 @@
 #-*- coding: utf-8 -*-
 
 import numpy as np
-from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 from sklearn.cross_validation import train_test_split
 from sklearn.datasets import load_digits
+from sklearn.grid_search import GridSearchCV
 
 #データの読み込み
 data = load_digits()
@@ -15,11 +16,8 @@ data_train, data_test, label_train, label_test = train_test_split(data.data,data
 
 #学習する
 print '学習開始'
-estimator = LinearSVC(C=1.0)
-estimator.fit(data_train, label_train)
-#予測する
-print '予測開始'
-label_predict = estimator.predict(data_test)
-#評価する
-print confusion_matrix(label_test,label_predict)
-print accuracy_score(label_test,label_predict)
+parameters = {'kernel':('linear', 'poly', 'rbf', 'sigmoid'), 'C':[0.1,1,10,100]}
+estimator = SVC()
+clf = GridSearchCV(estimator, parameters)
+clf.fit(data_train, label_train)
+print clf.grid_scores_
